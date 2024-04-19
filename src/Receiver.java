9709ab7c0481cf3e2cf62c65fde086faf9be81bf
masonrware <86129546+masonrware@users.noreply.github.com>
2024-04-19 15:48:56 -0500

    continuing to add skeleton infra to the receiver and sender

--- src/Receiver.java
+++ src/Receiver.java
@@ -12,6 +12,9 @@ public class Receiver {
     private int totalPacketsWithIncorrectChecksum = 0;
     private int totalDuplicateAcks = 0;
 
+    private int sequenceNumber = 0;
+    private int ackNumber = 0;
+
     public static void main(String[] args) {
         if (args.length != 8 || !args[0].equals("-p") || !args[2].equals("-m") || !args[4].equals("-c") || !args[6].equals("-f")) {
             System.out.println("Usage: java Receiver -p <port> -m <mtu> -c <sws> -f <file name>");
@@ -23,23 +26,66 @@ public class Receiver {
         int sws = Integer.parseInt(args[5]);
         String fileName = args[7];
 
-        // TODO: Implement receiver logic here
+        Receiver receiver = new Receiver();
+
+        try {
+            DatagramSocket socket = new DatagramSocket(port);
+            byte[] buffer = new byte[mtu];
+
+            // TCP Handshake Code
+            DatagramPacket synPacket = new DatagramPacket(buffer, buffer.length);
+            socket.receive(synPacket);
+            // Process SYN packet
+            if (receiver.isSYN(synPacket.getData())) {
+                InetAddress senderIP = synPacket.getAddress();
+                int senderPort = synPacket.getPort();
+                // Send SYN-ACK packet
+                receiver.sendSYNACK(socket, senderIP, senderPort);
+            }
+            // Wait for ACK from sender
+            DatagramPacket ackPacket = new DatagramPacket(buffer, buffer.length);
+            socket.receive(ackPacket);
+            // Process ACK packet
+            if (receiver.isACK(ackPacket.getData())) {
+
+                // Post handshake
+                
+                // TODO: we have to send and receive at the same time -- threading???
+                // Handshake successful, start receiving data segments
+                while (true) {
+                    DatagramPacket dataPacket = new DatagramPacket(buffer, buffer.length);
+                    socket.receive(dataPacket);
+
+                    // TODO: handle different types of potential packets -- (no need for syn, handled above), data/ack, fin, ack ...
+                    // Process received data segment
+                    // receiver.handleDataSegment(dataPacket);
+                }
+            } else {
+                // TODO: handshake not set up properly?
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    // Method to output segment information
-    private void outputSegmentInfo(String flagList, int seqNumber, int numBytes, int ackNumber) {
-        Date date = new Date();
-        System.out.printf("%d rcv %s %d %d %d\n", date.getTime(), flagList, seqNumber, numBytes, ackNumber);
+    /*
+     * HANDLERS (OUTSIDE OF HANDSHAKE)
+     */
+
+    // Method to handle ACK reception
+    private void handleACK(int ackNumber) {
+        // Implement ACK handling logic here
     }
 
     // Method to handle received data segment
-    private void handleDataSegment(DatagramPacket packet) {
-        // Implement handling logic here
-    }
+    private void handleDataSegment(DatagramSocket socket, InetAddress senderIP, int senderPort, byte[] data) throws IOException {
+        // TODO: Extract sequence number and data from the packet
+        // int sequenceNumber = ...; // Extract sequence number from the data portion of the packet
+        // byte[] data = ...; // Extract data from the packet
 
-    // Method to send ACK segment
-    private void sendACK(DatagramSocket socket, InetAddress senderIP, int senderPort, int ackNumber) {
-        // Implement sending ACK logic here
+        // Process received data, store in buffer, etc.
+        // Send ACK for the last successfully received contiguous byte
+        sendACK(socket, senderIP, senderPort, sequenceNumber + data.length);
     }
 
     // Method to handle FIN segment and close connection
@@ -47,8 +93,69 @@ public class Receiver {
         // Implement handling FIN logic here
     }
 
+    /*
+     * SENDERS
+     */
+
+    // Method to send UDP packet
+    private void sendPacket(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data) throws IOException {
+        DatagramPacket packet = new DatagramPacket(data, data.length, receiverIP, receiverPort);
+        socket.send(packet);
+        // Output information about the sent packet
+        outputSegmentInfo("D", sequenceNumber, data.length, -1);
+    }
+
+    private void sendSYNACK(DatagramSocket socket, InetAddress senderIP, int senderPort) throws IOException {
+        // TODO: Construct SYN-ACK packet data
+        byte[] synackData = { /* Construct SYN-ACK packet data */ };
+        // Send SYN-ACK packet
+        sendPacket(socket, senderIP, senderPort, synackData);
+    }
+
+    // Method to send ACK segment
+    private void sendACK(DatagramSocket socket, InetAddress senderIP, int senderPort, int ackNumber) throws IOException {
+            // Construct ACK packet data
+        byte[] ackData = { /* Construct ACK packet data */ };        
+        // Send the ACK packet
+        sendPacket(socket, senderIP, senderPort, ackData);
+    }
+
+    // Method to send FIN segment and close connection
+    private void sendFINACK(DatagramSocket socket, InetAddress receiverIP, int receiverPort) {
+        // Implement FIN segment sending logic here
+    }
+
+    /*
+     * MISC.
+     */
+
     // Method to close the connection and print statistics
     private void closeConnectionAndPrintStatistics() {
         // Implement closing logic and print statistics here
+    }
+    
+    // Method to output segment information
+    private void outputSegmentInfo(String flagList, int seqNumber, int numBytes, int ackNumber) {
+        Date date = new Date();
+        // Need both snd and rcv ability
+        System.out.printf("%d rcv %s %d %d %d\n", date.getTime(), flagList, seqNumber, numBytes, ackNumber);
+    }
+
+    private boolean isSYN(byte[] data) {
+        // Check if the packet is a SYN packet
+        // Implement logic to check if the packet is a SYN packet
+        return true; // Placeholder, actual implementation depends on protocol
+    }
+
+    private boolean isACK(byte[] data) {
+        // Check if the packet is a ACK packet
+        // Implement logic to check if the packet is a ACK packet
+        return true; // Placeholder, actual implementation depends on protocol
+    }
+
+    private boolean isFIN(byte[] data) {
+        // Check if the packet is a FIN packet
+        // Implement logic to check if the packet is a FIN packet
+        return true; // Placeholder, actual implementation depends on protocol
     }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -11,6 +11,9 @@ public class Sender {
     private int totalPacketsSent = 0;
     private int totalRetransmissions = 0;
 
+    private int sequenceNumber = 0;
+    private int ackNumber = 0;
+
     public static void main(String[] args) {
         if (args.length != 10 || !args[0].equals("-p") || !args[2].equals("-s") || !args[4].equals("-a")
                 || !args[6].equals("f") || !args[8].equals("-m") || !args[10].equals("-c")) {
@@ -25,19 +28,44 @@ public class Sender {
         int mtu = Integer.parseInt(args[9]);
         int sws = Integer.parseInt(args[11]);
 
-        // TODO: Implement sender logic here
+        Sender sender = new Sender();
+
+        try {
+            DatagramSocket socket = new DatagramSocket(port);
+            InetAddress remoteAddress = InetAddress.getByName(remoteIP);
+            byte[] buffer = new byte[mtu];
+
+            // TCP Handshake Code
+            // Send SYN packet
+            sender.sendSYN(socket, remoteAddress, remotePort);
+            // Wait for SYN-ACK from receiver
+            DatagramPacket synackPacket = new DatagramPacket(buffer, buffer.length);
+            socket.receive(synackPacket);
+            // Process SYN-ACK packet
+            if (sender.isSYNACK(synackPacket.getData())) {
+                // Send ACK to complete handshake
+                sender.sendACK(socket, remoteAddress, remotePort);
+            } else {
+                // TODO: handshake not set up properly?
+            }
+            
+            // Post handshake
+
+            // TODO: we have to send and receive at the same time -- threading???
+            // Start sending data segments
+            while (true) {
+                // TODO: Read data from file into buffer
+                // Call sendDataSegment method to send the data segment
+                sender.sendDataSegment(socket, remoteAddress, remotePort, buffer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    // Method to output segment information
-    private void outputSegmentInfo(String flagList, int seqNumber, int numBytes, int ackNumber) {
-        Date date = new Date();
-        System.out.printf("%d snd %s %d %s %d %d %d\n", date.getTime(), flagList, seqNumber, numBytes, ackNumber);
-    }
-
-    // Method to send data segment
-    private void sendDataSegment(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data, int sequenceNumber) {
-        // Implement sending logic here
-    }
+    /*
+     * HANDLERS (OUTSIDE OF HANDSHAKE)
+     */
 
     // Method to handle ACK reception
     private void handleACK(int ackNumber) {
@@ -45,8 +73,45 @@ public class Sender {
     }
 
     // Method to handle retransmissions
-    private void handleRetransmission(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data, int sequenceNumber) {
+    private void handleRetransmission(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data) {
         // Implement retransmission logic here
+    }
+
+    // Method to handle FIN segment and close connection
+    private void handleFIN(DatagramSocket socket, InetAddress senderIP, int senderPort) {
+        // Implement handling FIN logic here
+    }
+
+    /*
+     * SENDERS
+     */
+
+    // Method to send UDP packet
+    private void sendPacket(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data) throws IOException {
+        DatagramPacket packet = new DatagramPacket(data, data.length, receiverIP, receiverPort);
+        socket.send(packet);
+        // Output information about the sent packet
+        outputSegmentInfo("D", sequenceNumber, data.length, -1);
+    }
+
+    private void sendACK(DatagramSocket socket, InetAddress receiverIP, int receiverPort) throws IOException {
+        // Construct ACK packet data
+        byte[] ackData = { /* Construct ACK packet data */ };
+        // Send ACK packet
+        sendPacket(socket, receiverIP, receiverPort, ackData);
+    }
+
+
+    private void sendSYN(DatagramSocket socket, InetAddress receiverIP, int receiverPort) throws IOException {
+        // Construct SYN packet data
+        byte[] synData = { /* Construct SYN packet data */ };
+        // Send SYN packet
+        sendPacket(socket, receiverIP, receiverPort, synData);
+    }
+
+    // Method to send data segment
+    private void sendDataSegment(DatagramSocket socket, InetAddress receiverIP, int receiverPort, byte[] data) {
+        // Implement sending logic here
     }
 
     // Method to send FIN segment and close connection
@@ -54,8 +119,25 @@ public class Sender {
         // Implement FIN segment sending logic here
     }
 
+    /*
+     * MISC.
+     */
+
     // Method to close the connection and print statistics
     private void closeConnectionAndPrintStatistics() {
         // Implement closing logic and print statistics here
+    }
+    
+    // Method to output segment information
+    private void outputSegmentInfo(String flagList, int seqNumber, int numBytes, int ackNumber) {
+        Date date = new Date();
+        // Need both snd and rcv ability
+        System.out.printf("%d snd %s %d %s %d %d %d\n", date.getTime(), flagList, seqNumber, numBytes, ackNumber);
+    }
+
+    private boolean isSYNACK(byte[] data) {
+        // Check if the packet is a SYN-ACK packet
+        // Implement logic to check if the packet is a SYN-ACK packet
+        return true; // Placeholder, actual implementation depends on protocol
     }
 }
