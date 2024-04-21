c320fbb222926976538a5c9b7256c0c04af32c72
Ben Brandl <bbrandl2@wisc.edu>
2024-04-21 13:23:58 -0500

    UDP reference for setting up socket connection

--- src/TCPend.java
+++ src/TCPend.java
@@ -22,7 +22,9 @@ public class TCPend {
 
             Sender sender = new Sender(remotePort, remoteIP, port, fileName, mtu, sws);
 
-            // Send
+            // Set up socket inside Sender class, call method here to initiate data transfer
+                // Use DatagramSocket for UDP connection, does not create a stream
+                // https://www.baeldung.com/udp-in-java
         }
         else if (args.length == 9){     // Receiver
             if (!args[1].equals("-p") || !args[3].equals("-m") || !args[5].equals("-c") || !args[7].equals("-f")) {
