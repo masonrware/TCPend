ab620fc50d81b1b12a41582774f0f0a99c69dca9
masonrware <86129546+masonrware@users.noreply.github.com>
2024-04-26 17:41:37 -0500

    adding debug statement

--- assign4/src/Sender.java
+++ assign4/src/Sender.java
@@ -163,6 +163,7 @@ public class Sender {
                     // Check for expired retransmission timers
                     for (Map.Entry<Integer, Timer> entry : retransmissionTimers.entrySet()) {
                         Timer timer = entry.getValue();
+                        System.out.println("<<<THREAD SEES: " + entry.getKey());
                         if (!timer.isDead() && timer.hasExpired()) {
                             System.out.println("<<<THREAD IS RETRANSMITTING: " + entry.getKey());
                             resendPacket(entry.getKey());
